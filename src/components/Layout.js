73448cf52e034b2baabb2c3edf3f647ad3fa2bd3
Guillermo Díaz <diazgdev@192.168.0.192>
2023-02-24 23:44:43 -0600

    styling layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import '../styles/global.css'
 
 export default function Layout({children}) {
   return (
@@ -9,7 +10,7 @@ export default function Layout({children}) {
         {children}
       </div>
       <footer>
-        <p>Created by diazgdev, 2023</p>
+        <p>By @diazgdev - 2023</p>
       </footer>
     </div>
   )
